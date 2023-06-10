af5ebe4cb87ba1ee55f38a396c2d9a634f8629a0
Steve Phelps <phelps.sg@gmail.com>
2023-06-10 08:53:27 +0100

    OpenAIScalaClientException can be pattern matched against Retryable(_) and RetryHelpers uses exponential backoff

--- openai-client/src/main/scala/io/cequence/openaiscala/RetryHelpers.scala
+++ openai-client/src/main/scala/io/cequence/openaiscala/RetryHelpers.scala
@@ -1,17 +1,19 @@
 package io.cequence.openaiscala
 
 import akka.actor.{ActorSystem, Scheduler}
-import akka.pattern.RetrySupport
+import akka.pattern.{RetrySupport, after}
 import akka.stream.Materializer
 import io.cequence.openaiscala.RetryHelpers.RetrySettings
 
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 object RetryHelpers {
   final case class RetrySettings(
       maxRetries: Integer = 5,
-      delay: FiniteDuration = 2.seconds
+      delayBase: FiniteDuration = 2.seconds,
+      delayExponent: FiniteDuration = 2.seconds
   )
 }
 
@@ -21,12 +23,42 @@ trait RetryHelpers extends RetrySupport {
   implicit val materializer: Materializer = Materializer(actorSystem)
 
   implicit class FutureWithRetry[T](f: Future[T]) {
+
+    def delay(implicit retrySettings: RetrySettings): FiniteDuration =
+      retrySettings.delayBase + scala.math.pow(
+        2.0,
+        retrySettings.delayExponent.asInstanceOf[Double]
+      )
+
+    def retry[T](
+        attempt: () => Future[T],
+        attempts: Int
+    )(implicit
+        ec: ExecutionContext,
+        scheduler: Scheduler,
+        retrySettings: RetrySettings
+    ): Future[T] = {
+      try {
+        if (attempts > 0) {
+          attempt().recoverWith { case Retryable(_) =>
+            after(delay, scheduler) {
+              retry(attempt, attempts - 1)
+            }
+          }
+        } else {
+          attempt()
+        }
+      } catch {
+        case NonFatal(error) => Future.failed(error)
+      }
+    }
+
     def retryOnFailure(implicit
         retrySettings: RetrySettings,
         ec: ExecutionContext,
         scheduler: Scheduler
     ): Future[T] = {
-      retry(() => f, retrySettings.maxRetries, retrySettings.delay)
+      retry(() => f, retrySettings.maxRetries)
     }
   }
 }
--- openai-core/src/main/scala/io/cequence/openaiscala/OpenAIScalaClientException.scala
+++ openai-core/src/main/scala/io/cequence/openaiscala/OpenAIScalaClientException.scala
@@ -1,17 +1,34 @@
 package io.cequence.openaiscala
 
-class OpenAIScalaClientException(message: String, cause: Throwable) extends RuntimeException(message, cause) {
+object Retryable {
+
+  def unapply(
+      t: OpenAIScalaClientException
+  ): Option[OpenAIScalaClientException] = Some(t).filter(apply)
+
+  def apply(t: OpenAIScalaClientException): Boolean = t match {
+    case _: OpenAIScalaClientTimeoutException => true
+    case _                                    => false
+  }
+
+}
+
+class OpenAIScalaClientException(message: String, cause: Throwable)
+    extends RuntimeException(message, cause) {
   def this(message: String) = this(message, null)
 }
 
-class OpenAIScalaClientTimeoutException(message: String, cause: Throwable) extends OpenAIScalaClientException(message, cause) {
+class OpenAIScalaClientTimeoutException(message: String, cause: Throwable)
+    extends OpenAIScalaClientException(message, cause) {
   def this(message: String) = this(message, null)
 }
 
-class OpenAIScalaClientUnknownHostException(message: String, cause: Throwable) extends OpenAIScalaClientException(message, cause) {
+class OpenAIScalaClientUnknownHostException(message: String, cause: Throwable)
+    extends OpenAIScalaClientException(message, cause) {
   def this(message: String) = this(message, null)
 }
 
-class OpenAIScalaTokenCountExceededException(message: String, cause: Throwable) extends OpenAIScalaClientException(message, cause) {
+class OpenAIScalaTokenCountExceededException(message: String, cause: Throwable)
+    extends OpenAIScalaClientException(message, cause) {
   def this(message: String) = this(message, null)
 }
