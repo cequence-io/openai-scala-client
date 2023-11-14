ed4e261c1e05c7a1c24ca000770520bf90b839c0
peterbanda <peter.banda@protonmail.com>
2023-11-14 22:47:50 +0100

    Scala test for BaseMessage json serialization (around 17 cases)

---
+++ openai-client/src/test/scala/io/cequence/openaiscala/MessageJsonSpec.scala
@@ -0,0 +1,380 @@
+package io.cequence.openaiscala
+
+import io.cequence.openaiscala.domain._
+import io.cequence.openaiscala.JsonFormats.messageWrites
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import play.api.libs.json.{JsArray, JsNull, JsObject, JsString, Json}
+
+class MessageJsonSpec extends Matchers with AnyWordSpecLike {
+
+  "SystemMessage" should {
+    "serialize correctly with content only" in {
+      val content = "You are a helpful assistant."
+
+      val message = SystemMessage(content)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("system")
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with content and name" in {
+      val content =
+        "You are a brilliant mathematician. Take a deep breath and thing step by step."
+      val name = "euclid_123"
+
+      val message = SystemMessage(content, name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("system")
+      json("name") shouldBe JsString(name)
+      json("content") shouldBe JsString(content)
+    }
+  }
+
+  "UserMessage" should {
+    "serialize correctly with content only" in {
+      val content = "Why does it snow in Norway?"
+
+      val message = UserMessage(content)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("user")
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with content and name" in {
+      val content = "Why does it snow in Norway? I mean it's really far from South Pole."
+      val name = "euclid_123"
+
+      val message = UserMessage(content, name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("user")
+      json("name") shouldBe JsString(name)
+      json("content") shouldBe JsString(content)
+    }
+  }
+
+  "UserSeqMessage" should {
+    "serialize correctly with content only" in {
+      val content1 = TextContent("Why does it snow in Norway?")
+      val content2 = TextContent("I mean it's really far from South Pole.")
+      val content3 = ImageURLContent("https://www.visitnorway.com/")
+
+      val message = UserSeqMessage(Seq(content1, content2, content3))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("user")
+      json("content").asOpt[JsArray] shouldBe defined
+      json("content") shouldBe JsArray(
+        Seq(
+          Json.obj(
+            "type" -> "text",
+            "text" -> JsString(content1.text)
+          ),
+          Json.obj(
+            "type" -> "text",
+            "text" -> JsString(content2.text)
+          ),
+          Json.obj(
+            "type" -> "image_url",
+            "image_url" -> Json.obj("url" -> JsString(content3.url))
+          )
+        )
+      )
+    }
+
+    "serialize correctly with content and name" in {
+      val content1 = TextContent("Why does it snow in Norway?")
+      val content2 = TextContent("I mean it's really far from South Pole.")
+      val content3 = ImageURLContent("https://www.visitnorway.com/")
+      val name = "thor_23"
+
+      val message = UserSeqMessage(Seq(content1, content2, content3), name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("user")
+      json("name") shouldBe JsString(name)
+      json("content").asOpt[JsArray] shouldBe defined
+      json("content") shouldBe JsArray(
+        Seq(
+          Json.obj(
+            "type" -> "text",
+            "text" -> JsString(content1.text)
+          ),
+          Json.obj(
+            "type" -> "text",
+            "text" -> JsString(content2.text)
+          ),
+          Json.obj(
+            "type" -> "image_url",
+            "image_url" -> Json.obj("url" -> JsString(content3.url))
+          )
+        )
+      )
+    }
+  }
+
+  "AssistantMessage" should {
+    "serialize correctly with content only" in {
+      val content = "Norway is in the northern hemisphere."
+
+      val message = AssistantMessage(content)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("assistant")
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with content and name" in {
+      val content = "Norway is in the northern hemisphere, which is far from South Pole."
+      val name = "euclid_123"
+
+      val message = AssistantMessage(content, name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("assistant")
+      json("name") shouldBe JsString(name)
+      json("content") shouldBe JsString(content)
+    }
+  }
+
+  "AssistantToolMessage" should {
+    "serialize correctly with content only" in {
+      val content = "Norway is in the northern hemisphere."
+
+      val message = AssistantToolMessage(Some(content))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      jsonKeys shouldNot contain("tool_calls") // tool_calls must be excluded
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("assistant")
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with content and name" in {
+      val content = "Norway is in the northern hemisphere, which is far from South Pole."
+      val name = "euclid_123"
+
+      val message = AssistantToolMessage(Some(content), name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      jsonKeys shouldNot contain("tool_calls") // tool_calls must be excluded
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("assistant")
+      json("name") shouldBe JsString(name)
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with tool_calls" in {
+      val toolId = "get_current_weather_1"
+      val toolCallSpec = FunctionCallSpec(
+        "get_current_weather",
+        arguments = "{\n  \"location\": \"Boston, MA\"\n}"
+      )
+
+      val message = AssistantToolMessage(tool_calls = Seq(("get_current_weather_1", toolCallSpec)))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys shouldNot contain("content")
+      jsonKeys shouldNot contain("name")
+      jsonKeys should contain("tool_calls")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("assistant")
+      json("tool_calls").asOpt[JsArray] shouldBe defined
+      json("tool_calls") shouldBe JsArray(Seq(
+        Json.obj(
+          "id" -> JsString(toolId),
+          "type" -> JsString("function"),
+          "function" -> Json.obj("name" -> JsString(toolCallSpec.name), "arguments" -> JsString(toolCallSpec.arguments))
+        )
+      ))
+    }
+  }
+
+  "AssistantFunMessage" should {
+    "serialize correctly with content only" in {
+      val content = "Norway is in the northern hemisphere."
+
+      val message = AssistantFunMessage(Some(content))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys shouldNot contain("name")
+      jsonKeys shouldNot contain("function_call") // function_call must be excluded
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("assistant")
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with content and name" in {
+      val content = "Norway is in the northern hemisphere, which is far from South Pole."
+      val name = "euclid_123"
+
+      val message = AssistantFunMessage(Some(content), name = Some(name))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      jsonKeys shouldNot contain("function_call") // function_call must be excluded
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("assistant")
+      json("name") shouldBe JsString(name)
+      json("content") shouldBe JsString(content)
+    }
+
+    "serialize correctly with tool_calls" in {
+      val functionCallSpec = FunctionCallSpec(
+        "get_current_weather",
+        arguments = "{\n  \"location\": \"Boston, MA\"\n}"
+      )
+
+      val message = AssistantFunMessage(function_call = Some(functionCallSpec))
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys shouldNot contain("content")
+      jsonKeys shouldNot contain("name")
+      jsonKeys should contain("function_call")
+      json.size shouldBe 2
+
+      json("role") shouldBe JsString("assistant")
+      json("function_call").asOpt[JsObject] shouldBe defined
+      json("function_call") shouldBe Json.obj("name" -> JsString(functionCallSpec.name), "arguments" -> JsString(functionCallSpec.arguments))
+    }
+  }
+
+  "ToolMessage" should {
+    "serialize correctly with tool_call_id and (tool/function) name" in {
+      val callId = "get_current_weather_1"
+      val functionName = "get_current_weather"
+
+      val message = ToolMessage(tool_call_id = callId, name = functionName)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys shouldNot contain("content")
+      jsonKeys should contain("tool_call_id")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("tool")
+      json("tool_call_id") shouldBe JsString(callId)
+      json("name") shouldBe JsString(functionName)
+    }
+
+    "serialize correctly with tool_call_id, (tool/function) name, and content (tool/function response)" in {
+      val callId = "get_current_weather_1"
+      val functionName = "get_current_weather"
+      val content = "It's raining in Seattle. It's 50 degrees Fahrenheit."
+
+      val message = ToolMessage(Some(content), tool_call_id = callId, name = functionName)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("tool_call_id")
+      jsonKeys should contain("name")
+      json.size shouldBe 4
+
+      json("role") shouldBe JsString("tool")
+      json("content") shouldBe JsString(content)
+      json("tool_call_id") shouldBe JsString(callId)
+      json("name") shouldBe JsString(functionName)
+    }
+  }
+
+  "FunMessage" should {
+    "serialize correctly with content (function response) and (function) name" in {
+      val content = "It's raining in Seattle. It's 50 degrees Fahrenheit."
+      val functionName = "get_current_weather"
+
+      val message = FunMessage(content, name = functionName)
+      val json = toJson(message)
+      val jsonKeys = json.keySet
+
+      jsonKeys should contain("role")
+      jsonKeys should contain("content")
+      jsonKeys should contain("name")
+      json.size shouldBe 3
+
+      json("role") shouldBe JsString("function")
+      json("content") shouldBe JsString(content)
+      json("name") shouldBe JsString(functionName)
+    }
+  }
+
+  private def toJson(message: BaseMessage) = Json.toJson(message).as[JsObject].value
+}
