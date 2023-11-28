98689fa76b8aa98c89410a3542ced15466a1001a
peterbanda <peter.banda@protonmail.com>
2023-11-28 22:20:44 +0100

    New examples - CreateAudioTranscription, CreateAudioTranslation (with an example mp3), and CreateAudioSpeech

--- openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateAudioTranscription.scala
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateAudioTranscription.scala
@@ -3,9 +3,9 @@ package io.cequence.openaiscala.examples
 object CreateAudioTranscription extends Example {
 
   override protected def run =
-    service.createAudioTranscription(
-      new java.io.File("speech-test.mp3")
-    ).map( response =>
-      println(response.text)
-    )
+    service
+      .createAudioTranscription(
+        new java.io.File("speech-test.mp3")
+      )
+      .map(response => println(response.text))
 }
