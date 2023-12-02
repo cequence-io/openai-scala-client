4766ea16d457252c1b17152e479b706be3b1ca05
peterbanda <peter.banda@protonmail.com>
2023-12-02 21:58:39 +0100

    Formatting

--- openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateThread.scala
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateThread.scala
@@ -9,7 +9,7 @@ object CreateThread extends Example {
       thread <- service.createThread(
         messages = Seq(
           ThreadMessage("Hello, what is AI?"), // file_ids = Seq("file-abc123")
-          ThreadMessage("How does AI work? Explain it in simple terms."),
+          ThreadMessage("How does AI work? Explain it in simple terms.")
         ),
         metadata = Map("user_id" -> "986413")
       )
