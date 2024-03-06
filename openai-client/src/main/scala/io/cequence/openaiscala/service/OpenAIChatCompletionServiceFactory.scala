0768ee06cf2cd8b4c92335619145192b1dd1179b
peterbanda <peter.banda@protonmail.com>
2024-03-06 10:34:46 +0100

    Impl and factory for OpenAIChatCompletionService

---
+++ openai-client/src/main/scala/io/cequence/openaiscala/service/OpenAIChatCompletionServiceFactory.scala
@@ -0,0 +1,30 @@
+package io.cequence.openaiscala.service
+
+import akka.stream.Materializer
+import io.cequence.openaiscala.service.ws.Timeouts
+
+import scala.concurrent.ExecutionContext
+
+object OpenAIChatCompletionServiceFactory {
+
+  def apply(
+    coreUrl: String,
+    authHeaders: Seq[(String, String)] = Nil,
+    extraParams: Seq[(String, String)] = Nil,
+    timeouts: Option[Timeouts] = None
+  )(
+    implicit ec: ExecutionContext,
+    materializer: Materializer
+  ): OpenAIChatCompletionService =
+    new OpenAIChatCompletionServiceClassImpl(coreUrl, authHeaders, extraParams, timeouts)
+}
+
+private class OpenAIChatCompletionServiceClassImpl(
+  val coreUrl: String,
+  val authHeaders: Seq[(String, String)],
+  val extraParams: Seq[(String, String)],
+  val explTimeouts: Option[Timeouts]
+)(
+  implicit val ec: ExecutionContext,
+  val materializer: Materializer
+) extends OpenAIChatCompletionServiceImpl
---
+++ openai-client/src/main/scala/io/cequence/openaiscala/service/OpenAIChatCompletionServiceImpl.scala
@@ -0,0 +1,71 @@
+package io.cequence.openaiscala.service
+
+import io.cequence.openaiscala.JsonFormats._
+import io.cequence.openaiscala.JsonUtil.JsonOps
+import io.cequence.openaiscala.domain.BaseMessage
+import io.cequence.openaiscala.domain.response._
+import io.cequence.openaiscala.domain.settings._
+import play.api.libs.json.{JsValue, Json}
+
+import scala.concurrent.Future
+
+/**
+ * Private impl. of [[OpenAIChatCompletionService]].
+ *
+ * @since Match
+ *   2024
+ */
+private trait OpenAIChatCompletionServiceImpl
+    extends OpenAIChatCompletionService
+    with OpenAIServiceWSHelper {
+
+  override def createChatCompletion(
+    messages: Seq[BaseMessage],
+    settings: CreateChatCompletionSettings
+  ): Future[ChatCompletionResponse] =
+    execPOST(
+      EndPoint.chat_completions,
+      bodyParams = createBodyParamsForChatCompletion(messages, settings, stream = false)
+    ).map(
+      _.asSafe[ChatCompletionResponse]
+    )
+
+  protected def createBodyParamsForChatCompletion(
+    messages: Seq[BaseMessage],
+    settings: CreateChatCompletionSettings,
+    stream: Boolean
+  ): Seq[(Param, Option[JsValue])] = {
+    assert(messages.nonEmpty, "At least one message expected.")
+
+    val messageJsons = messages.map(Json.toJson(_)(messageWrites))
+
+    jsonBodyParams(
+      Param.messages -> Some(messageJsons),
+      Param.model -> Some(settings.model),
+      Param.temperature -> settings.temperature,
+      Param.top_p -> settings.top_p,
+      Param.n -> settings.n,
+      Param.stream -> Some(stream),
+      Param.stop -> {
+        settings.stop.size match {
+          case 0 => None
+          case 1 => Some(settings.stop.head)
+          case _ => Some(settings.stop)
+        }
+      },
+      Param.max_tokens -> settings.max_tokens,
+      Param.presence_penalty -> settings.presence_penalty,
+      Param.frequency_penalty -> settings.frequency_penalty,
+      Param.logit_bias -> {
+        if (settings.logit_bias.isEmpty) None else Some(settings.logit_bias)
+      },
+      Param.user -> settings.user,
+      Param.logprobs -> settings.logprobs,
+      Param.top_logprobs -> settings.top_logprobs,
+      Param.seed -> settings.seed,
+      Param.response_format -> settings.response_format_type.map { formatType =>
+        Map("type" -> formatType.toString)
+      }
+    )
+  }
+}
