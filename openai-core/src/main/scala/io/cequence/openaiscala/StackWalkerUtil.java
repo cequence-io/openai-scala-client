2852a2d1cafb247556f2c0ede6d5be242b840eef
peterbanda <peter.banda@protonmail.com>
2024-03-07 14:19:40 +0100

    Predicate support for StackWalkerUtil

--- openai-core/src/main/scala/io/cequence/openaiscala/StackWalkerUtil.java
+++ openai-core/src/main/scala/io/cequence/openaiscala/StackWalkerUtil.java
@@ -1,16 +1,22 @@
 package io.cequence.openaiscala;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 
 // this requires Java 9+
 public class StackWalkerUtil {
 
   private static StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
-  public static Optional<String> functionName(int skip) {
+  public static Optional<String> functionName(
+    int skip,
+    Optional<Predicate<String>> predicate
+  ) {
     return walker.walk(frames -> frames
             .map(StackWalker.StackFrame::getMethodName)
             .skip(skip)
-            .findFirst());
+            .filter(predicate.orElse(s -> true))
+            .findFirst()
+    );
   }
 }
