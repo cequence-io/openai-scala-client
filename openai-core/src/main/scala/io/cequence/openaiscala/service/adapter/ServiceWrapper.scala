59de35ed5d703ba2215510f0c04af564a6a8eb3a
peterbanda <peter.banda@protonmail.com>
2024-03-07 14:20:35 +0100

    Richer hierarchy of service wrappers and delegators introduced

--- openai-core/src/main/scala/io/cequence/openaiscala/service/adapter/ServiceWrapper.scala
+++ openai-core/src/main/scala/io/cequence/openaiscala/service/adapter/ServiceWrapper.scala
@@ -1,10 +1,48 @@
 package io.cequence.openaiscala.service.adapter
 
+import io.cequence.openaiscala.domain.BaseMessage
+import io.cequence.openaiscala.domain.response.ChatCompletionResponse
+import io.cequence.openaiscala.domain.settings.CreateChatCompletionSettings
+import io.cequence.openaiscala.service.{CloseableService, OpenAIChatCompletionService}
+import io.cequence.openaiscala.service.adapter.ServiceWrapperTypes._
+
 import scala.concurrent.Future
 
-trait ServiceWrapper[S] extends S {
+trait ServiceWrapper[+S] {
 
-  protected def wrap[T](
+  protected[adapter] def wrap[T](
     fun: S => Future[T]
   ): Future[T]
+}
+
+trait DelegatedCloseableServiceWrapper[
+  +S <: CloseableService,
+  +W <: CloseableServiceWrapper[S]
+] extends ServiceWrapper[S]
+    with CloseableService {
+
+  protected def delegate: W
+
+  protected[adapter] def wrap[T](
+    fun: S => Future[T]
+  ): Future[T] = delegate.wrap(fun)
+
+  override def close(): Unit =
+    delegate.close()
+}
+
+trait DelegatedChatCompletionCloseableServiceWrapper[+S <: CloseableService]
+    extends DelegatedCloseableServiceWrapper[S, ChatCompletionCloseableServiceWrapper[S]]
+    with OpenAIChatCompletionService {
+
+  override def createChatCompletion(
+    messages: Seq[BaseMessage],
+    settings: CreateChatCompletionSettings
+  ): Future[ChatCompletionResponse] = delegate.createChatCompletion(messages, settings)
+}
+
+object ServiceWrapperTypes {
+  type CloseableServiceWrapper[+S] = ServiceWrapper[S] with CloseableService
+  type ChatCompletionCloseableServiceWrapper[+S] = CloseableServiceWrapper[S]
+    with OpenAIChatCompletionService
 }
