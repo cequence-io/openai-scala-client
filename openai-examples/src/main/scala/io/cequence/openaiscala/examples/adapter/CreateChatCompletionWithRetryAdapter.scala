380fc0662643997e1229ab02087ccd52fe373361
peterbanda <peter.banda@protonmail.com>
2024-03-13 14:41:01 +0100

    Retry adapter example

---
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/adapter/CreateChatCompletionWithRetryAdapter.scala
@@ -0,0 +1,75 @@
+package io.cequence.openaiscala.examples.adapter
+
+import akka.actor.Scheduler
+import io.cequence.openaiscala.{OpenAIScalaClientException, OpenAIScalaClientTimeoutException}
+import io.cequence.openaiscala.RetryHelpers.RetrySettings
+import io.cequence.openaiscala.domain.settings.CreateChatCompletionSettings
+import io.cequence.openaiscala.domain.{BaseMessage, ModelId, SystemMessage, UserMessage}
+import io.cequence.openaiscala.examples.ExampleBase
+import io.cequence.openaiscala.service._
+import io.cequence.openaiscala.service.adapter.OpenAIServiceAdapters
+
+import scala.concurrent.Future
+
+object CreateChatCompletionWithRetryAdapter extends ExampleBase[OpenAIService] {
+
+  // adapters to use (round-robin, retry, etc.)
+  private val adapters = OpenAIServiceAdapters.forFullService
+
+  // implicit retry settings and scheduler
+  private implicit val retrySettings: RetrySettings = RetrySettings(maxRetries = 4)
+  private implicit val scheduler: Scheduler = system.scheduler
+
+  // regular OpenAI service
+  private val regularService = OpenAIServiceFactory()
+
+  // to demonstrate the retry mechanism we introduce a service that always times out
+  private val failingService = adapters.preAction(
+    OpenAIServiceFactory(),
+    () => Future(throw new OpenAIScalaClientTimeoutException("Fake timeout"))
+  )
+
+  // we then map the failing service to a specific model - gpt-3.5-turbo-1106
+  // for all other models we use the regular service
+  private val mergedService = adapters.chatCompletion(
+    serviceModels = Map(failingService -> Seq(ModelId.gpt_3_5_turbo_1106)),
+    regularService
+  )
+
+  // and finally we apply the retry mechanism to the merged service
+  override val service: OpenAIService = adapters.retry(
+    mergedService,
+    Some(println(_)) // simple logging
+  )
+
+  private val messages = Seq(
+    SystemMessage("You are a helpful assistant."),
+    UserMessage("What is the weather like in Norway?")
+  )
+
+  override protected def run: Future[_] =
+    for {
+      // this invokes the failing service, which triggers the retry mechanism
+      _ <- runChatCompletionAux(ModelId.gpt_3_5_turbo_1106).recover {
+        case e: OpenAIScalaClientException =>
+          println(s"Too many retries, giving up on '${e.getMessage}'")
+      }
+
+      // should complete without retry
+      _ <- runChatCompletionAux(ModelId.gpt_3_5_turbo_0125)
+    } yield ()
+
+  private def runChatCompletionAux(model: String) = {
+    println(s"Running chat completion with the model '$model'\n")
+
+    service
+      .createChatCompletion(
+        messages = messages,
+        settings = CreateChatCompletionSettings(model)
+      )
+      .map { response =>
+        printMessageContent(response)
+        println("--------")
+      }
+  }
+}
