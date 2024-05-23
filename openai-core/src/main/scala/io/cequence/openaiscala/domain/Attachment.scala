e82cf805f08f5066f97a7f6bf00c293fe74dbff7
Boris Burdiliak <boris.burdiliak@gmail.com>
2024-05-23 13:09:01 +0200

    add unapply returning Options to pass compilation for Scala 3

--- openai-core/src/main/scala/io/cequence/openaiscala/domain/Attachment.scala
+++ openai-core/src/main/scala/io/cequence/openaiscala/domain/Attachment.scala
@@ -1,8 +1,15 @@
 package io.cequence.openaiscala.domain
 
+import scala.collection.immutable.Seq
+
 final case class Attachment(
   // The ID of the file to attach to the message.
   fileId: Option[FileId],
   // The tools to add this file to.
   tools: Seq[MessageTool] = Nil
 )
+
+object Attachment {
+  def unapply(attachment: Attachment): Option[(Option[FileId], Seq[MessageTool])] =
+    Some((attachment.fileId, attachment.tools))
+}
