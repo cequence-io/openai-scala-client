dfba06c2b58a9fb2a6c6496a9c2c45d8d39c0f9d
Boris Burdiliak <boris.burdiliak@gmail.com>
2024-08-15 16:59:35 +0200

    https://portal.app.cequence.io/#/projects/3264 format

---
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateChatCompletionLongResponse.scala
@@ -0,0 +1,27 @@
+package io.cequence.openaiscala.examples
+
+import io.cequence.openaiscala.domain._
+import io.cequence.openaiscala.domain.settings.CreateChatCompletionSettings
+
+import scala.concurrent.Future
+
+object CreateChatCompletionLongResponse extends Example {
+
+  val messages = Seq(
+    SystemMessage("You are a helpful assistant."),
+    UserMessage("What is the weather like in Norway?")
+  )
+
+  override protected def run: Future[_] =
+    service
+      .createChatCompletion(
+        messages = messages,
+        settings = CreateChatCompletionSettings(
+          model = ModelId.gpt_4o,
+          temperature = Some(0)
+        )
+      )
+      .map { content =>
+        printMessageContent(content)
+      }
+}
