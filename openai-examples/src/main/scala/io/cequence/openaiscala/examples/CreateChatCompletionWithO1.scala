cc27bc0e35430db9c9ac1248eff86f812e5ea91d
Peter Banda <peter.banda@protonmail.com>
2025-01-28 15:43:51 +0100

    O1 reasoning effort example

--- openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateChatCompletionWithO1.scala
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/CreateChatCompletionWithO1.scala
@@ -1,10 +1,7 @@
 package io.cequence.openaiscala.examples
 
 import io.cequence.openaiscala.domain._
-import io.cequence.openaiscala.domain.settings.{
-  ChatCompletionResponseFormatType,
-  CreateChatCompletionSettings
-}
+import io.cequence.openaiscala.domain.settings.{ChatCompletionResponseFormatType, CreateChatCompletionSettings, ReasoningEffort}
 
 import scala.concurrent.Future
 
@@ -16,7 +13,9 @@ object CreateChatCompletionWithO1 extends Example {
     UserMessage("What is the weather like in Norway per major cities? Answer in json format.")
   )
 
-  override protected def run: Future[_] =
+  override protected def run: Future[_] = {
+    val start = new java.util.Date()
+
     service
       .createChatCompletion(
         messages = messages,
@@ -24,10 +23,14 @@ object CreateChatCompletionWithO1 extends Example {
           model = ModelId.o1,
           temperature = Some(0.1),
           response_format_type = Some(ChatCompletionResponseFormatType.json_object),
-          max_tokens = Some(4000)
+          max_tokens = Some(10000),
+          reasoning_effort = Some(ReasoningEffort.low)
         )
       )
       .map { content =>
+        println(s"Time taken: ${new java.util.Date().getTime - start.getTime} ms")
+        println("Usage: " + content.usage.get)
         printMessageContent(content)
       }
+  }
 }
