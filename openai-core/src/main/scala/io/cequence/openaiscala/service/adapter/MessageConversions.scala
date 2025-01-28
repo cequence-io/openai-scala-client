8ca767882942889588167b0402f787d0d226c083
Peter Banda <peter.banda@protonmail.com>
2025-01-28 15:41:03 +0100

    Message conversion - Deepseek thinking tokens filtration (non-streamed and streamed)

--- openai-core/src/main/scala/io/cequence/openaiscala/service/adapter/MessageConversions.scala
+++ openai-core/src/main/scala/io/cequence/openaiscala/service/adapter/MessageConversions.scala
@@ -1,15 +1,18 @@
 package io.cequence.openaiscala.service.adapter
 
-import io.cequence.openaiscala.domain.{BaseMessage, SystemMessage, UserMessage}
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+import io.cequence.openaiscala.domain.response.ChunkMessageSpec
+import io.cequence.openaiscala.domain.{AssistantMessage, BaseMessage, SystemMessage, UserMessage}
 import org.slf4j.LoggerFactory
 
 object MessageConversions {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-  type MessageConversion = Seq[BaseMessage] => Seq[BaseMessage]
+  type MessagesConversion = Seq[BaseMessage] => Seq[BaseMessage]
 
-  val systemToUserMessages: MessageConversion =
+  val systemToUserMessages: MessagesConversion =
     (messages: Seq[BaseMessage]) => {
       val nonSystemMessages = messages.map {
         case SystemMessage(content, _) =>
@@ -34,4 +37,31 @@ object MessageConversions {
         case (acc, message) => acc :+ message
       }
     }
+
+  lazy val thinkEndTagRegex = "(?<!['\"])</think>(?!['\"])"
+
+  val filterOutToThinkEnd: AssistantMessage => AssistantMessage =
+    (message: AssistantMessage) => {
+      val newContent = message.content.split(thinkEndTagRegex).last.trim
+      message.copy(content = newContent)
+    }
+
+  def filterOutToThinkEndFlow: Flow[Seq[ChunkMessageSpec], Seq[ChunkMessageSpec], NotUsed] = {
+    Flow[Seq[ChunkMessageSpec]].statefulMapConcat { () =>
+      var foundEnd = false
+
+      (messages: Seq[ChunkMessageSpec]) => {
+        if (foundEnd) {
+          List(messages)
+        } else {
+          val endFoundInThisChunk = messages.exists(_.content.exists(_.trim.matches(thinkEndTagRegex)))
+
+          if (endFoundInThisChunk) {
+            foundEnd = true
+          }
+          List(messages.map(_.copy(content = None)))
+        }
+      }
+    }
+  }
 }
