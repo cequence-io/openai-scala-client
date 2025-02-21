72b2be584636cc01d5967e5fa037ab45bd5aca0f
Peter Banda <peter.banda@protonmail.com>
2025-02-21 21:08:56 +0100

    Usage info - a few attributes made optional

--- openai-core/src/main/scala/io/cequence/openaiscala/domain/response/TextCompletionResponse.scala
+++ openai-core/src/main/scala/io/cequence/openaiscala/domain/response/TextCompletionResponse.scala
@@ -23,18 +23,20 @@ case class UsageInfo(
   total_tokens: Int,
   completion_tokens: Option[Int],
   prompt_tokens_details: Option[PromptTokensDetails] = None,
-  completion_tokens_details: Option[CompletionTokenDetails] = None
+  completion_tokens_details: Option[CompletionTokenDetails] = None,
+//  prompt_cache_hit_tokens: Option[Int],
+//  prompt_cache_miss_tokens: Option[Int]
 )
 
 case class CompletionTokenDetails(
   reasoning_tokens: Int,
-  accepted_prediction_tokens: Int,
-  rejected_prediction_tokens: Int
+  accepted_prediction_tokens: Option[Int],
+  rejected_prediction_tokens: Option[Int]
 )
 
 case class PromptTokensDetails(
   cached_tokens: Int,
-  audio_tokens: Int
+  audio_tokens: Option[Int]
 )
 
 case class LogprobsInfo(
