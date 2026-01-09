bfbb90c3c478efbfdf6316b1310330aa8eca9ed3
Peter Banda <peter.banda@protonmail.com>
2026-01-09 13:46:19 +0100

    Anthropic bedrock example for EU (cross-region inference)

---
+++ openai-examples/src/main/scala/io/cequence/openaiscala/examples/anthropic/AnthropicBedrockCreateChatCompletionInEUWithOpenAIAdapter.scala
@@ -0,0 +1,33 @@
+package io.cequence.openaiscala.examples.anthropic
+
+import io.cequence.openaiscala.domain.settings.CreateChatCompletionSettings
+import io.cequence.openaiscala.domain.{NonOpenAIModelId, SystemMessage, UserMessage}
+import io.cequence.openaiscala.examples.{ChatCompletionProvider, ExampleBase}
+import io.cequence.openaiscala.service.OpenAIChatCompletionService
+
+import scala.concurrent.Future
+
+// requires `openai-scala-anthropic-client` as a dependency and 'AWS_BEDROCK_ACCESS_KEY', 'AWS_BEDROCK_SECRET_KEY', 'AWS_BEDROCK_REGION' environment variable to be set
+object AnthropicBedrockCreateChatCompletionInEUWithOpenAIAdapter
+    extends ExampleBase[OpenAIChatCompletionService] {
+
+  override val service: OpenAIChatCompletionService = ChatCompletionProvider.anthropicBedrock
+
+  private val messages = Seq(
+    SystemMessage("You are a drunk assistant!"),
+    UserMessage("What is the weather like in Norway?")
+  )
+
+  private val modelId =
+    "eu." + NonOpenAIModelId.bedrock_claude_sonnet_4_5_20250929_v1_0
+
+  override protected def run: Future[_] =
+    service
+      .createChatCompletion(
+        messages = messages,
+        settings = CreateChatCompletionSettings(modelId)
+      )
+      .map { content =>
+        println(content.contentHead)
+      }
+}
