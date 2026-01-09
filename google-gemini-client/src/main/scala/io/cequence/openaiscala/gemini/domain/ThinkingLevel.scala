a3512638b5ec7a83d5aa68bcef311f0fb574f15e
Peter Banda <peter.banda@protonmail.com>
2026-01-09 13:37:28 +0100

    Gemini - thinking level : minimal

--- google-gemini-client/src/main/scala/io/cequence/openaiscala/gemini/domain/ThinkingLevel.scala
+++ google-gemini-client/src/main/scala/io/cequence/openaiscala/gemini/domain/ThinkingLevel.scala
@@ -6,11 +6,13 @@ sealed trait ThinkingLevel extends EnumValue
 
 object ThinkingLevel {
   case object THINKING_LEVEL_UNSPECIFIED extends ThinkingLevel
+  case object MINIMAL extends ThinkingLevel // Gemini 3 Flash only
   case object LOW extends ThinkingLevel
   case object HIGH extends ThinkingLevel
 
   def values: Seq[ThinkingLevel] = Seq(
     THINKING_LEVEL_UNSPECIFIED,
+    MINIMAL,
     LOW,
     HIGH
   )
